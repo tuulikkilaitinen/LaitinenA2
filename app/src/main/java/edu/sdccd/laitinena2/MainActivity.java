29c59deb6aa4e39290eb3b652d4880899a379d03
Tuulikki Laitinen <tuulikki.laitinen@gmail.com>
2017-02-25 17:55:39 -0800

    Initial commit

---
+++ app/src/main/java/edu/sdccd/laitinena2/MainActivity.java
@@ -0,0 +1,19 @@
+package edu.sdccd.laitinena2;
+
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
+import android.media.Rating;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.RatingBar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
